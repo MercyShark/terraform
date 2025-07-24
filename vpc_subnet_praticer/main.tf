2793f152716de7c5c6bf7c0a21fad27943d91b61
Rishabh <prajapatirishabh04@gmail.com>
2025-07-24 17:56:11 +0530

    Add subnet resources for public and private subnets in VPC configuration

--- vpc_subnet_praticer/main.tf
+++ vpc_subnet_praticer/main.tf
@@ -15,11 +15,59 @@ variable "vpc_cidr_block_address" {
   description = "VPC cidr block for Infra"
 }
 
+variable "public_subnet_a_cidr_block" {}
+variable "public_subnet_b_cidr_block" {}
+variable "private_subnet_a_cidr_block" {}
+variable "private_subnet_b_cidr_block" {}
+
+
 resource "aws_vpc" "my_custom_vpc" {
     cidr_block = var.vpc_cidr_block_address
     tags = {
         Name = "my_custom_vpc_made_with_terraform"
     }
+}
+
+resource "aws_subnet" "public_subnet_A" { 
+  cidr_block =  var.public_subnet_a_cidr_block
+  vpc_id = aws_vpc.my_custom_vpc.id
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "name" = "Public Subnet A" 
+  }
+  
+}
+
+
+resource "aws_subnet" "public_subnet_B" { 
+  cidr_block =  var.public_subnet_b_cidr_block
+  vpc_id = aws_vpc.my_custom_vpc.id
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    "name" = "Public Subnet B" 
+  }
+}
+
+
+resource "aws_subnet" "private_subnet_A" { 
+  cidr_block =  var.private_subnet_a_cidr_block
+  vpc_id = aws_vpc.my_custom_vpc.id
+  availability_zone = "ap-south-1a"
+  tags = {
+    "name" = "Private Subnet A" 
+  }
+}
+
+resource "aws_subnet" "private_subnet_B" { 
+  cidr_block =  var.private_subnet_b_cidr_block
+  vpc_id = aws_vpc.my_custom_vpc.id
+  availability_zone = "ap-south-1b"
+  tags = {
+    "name" = "Private Subnet B" 
+  }
 }
 
 output "my_custom_vpc_cird_block" {
